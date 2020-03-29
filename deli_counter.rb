64238a5f29c10c6ba39bed43bd82d30aa4c5d1f8
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-29 06:32:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,25 +10,18 @@
     puts "The line is currently empty."
   end
 end
- def take_a_number(katz_deli, "Ada") 
-   
-   
-   
-   
-   #=> Welcome, Ada. You are number 1 in line.
-  take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
-  take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+
  
-  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
- 
-  now_serving(katz_deli) #=> "Currently serving Ada."
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
- 
-  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
- 
-  now_serving(katz_deli) #=> "Currently serving Grace."
- 
-  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end
